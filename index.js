1f1335c4dd0ff56452b0259a9d347662a6282db8
Manan Patel <mjpatel@umass.edu>
2020-09-27 19:12:31 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'bro'});
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
