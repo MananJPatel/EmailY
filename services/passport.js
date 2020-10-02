950ad57fdcaeec54fb24243ba6dec16a52fd5caf
Manan Patel <mjpatel@umass.edu>
2020-10-02 09:18:43 -0400

    master

--- services/passport.js
+++ services/passport.js
@@ -11,7 +11,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    User.findByID(id).then(user => {
+    User.findById(id).then(user => {
         done(null,user);
     });
 });
