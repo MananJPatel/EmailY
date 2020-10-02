74e76289dafc58eb56757637d884ba2d45d15bbb
Manan Patel <mjpatel@umass.edu>
2020-10-02 09:35:56 -0400

    change

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,3 @@
-// module.exports = {
-//     googleClientID: '577002872517-ign35sbk1f2fe6if9lepsql8so0d286s.apps.googleusercontent.com',
-//     googleClientSecret: 'qPrnk_1Pd30CPmpWYGdq8dt1',
-//     mongoURI: 'mongodb+srv://p00gz:eATYhgRkLx9nTpng@emaily.bbz7d.mongodb.net/test?retryWrites=true&w=majority',
-//     cookieKey: 'qwertyuiosdfghjkldfghjkertyuicvbnmdfghjdfghjkdfghjdfghjcvbnsdfghsdfgh'
-// };
-
 module.exports = {
     googleClientID: 'process.env.GOOGLE_CLIENT_ID',
     googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
