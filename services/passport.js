60069bd5532914dcf7a68a8e24d5560e3b395380
Manan Patel <mjpatel@umass.edu>
2020-10-02 11:07:29 -0400

    Add existing file

--- services/passport.js
+++ services/passport.js
@@ -11,7 +11,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
+    User.findById(id).then(user => { 
         done(null,user);
     });
 });
