afeee26d745d9015ba167646d3f6e41bace497d7
Manan Patel <mjpatel@umass.edu>
2020-10-05 21:47:40 -0400

    routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ module.exports = app => {
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);
+        res.send(req.user);// routes
     })
 
     app.get('/api/current_user', (req, res) => {
